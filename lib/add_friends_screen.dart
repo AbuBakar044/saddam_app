3f8942ef4936389b297651b85777fd106e776305
AbuBakar044 <bhattiabubakar@gmail.com>
2023-10-30 14:34:31 +0500

    added validators, and forms
    added validators, and forms

--- lib/add_friends_screen.dart
+++ lib/add_friends_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -12,6 +13,7 @@ class AddFriendsScreen extends StatefulWidget {
 
 class _AddFriendsScreenState extends State<AddFriendsScreen> {
   Uint8List? userImage;
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,68 +28,107 @@ class _AddFriendsScreenState extends State<AddFriendsScreen> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                InkWell(
-                  onTap: () {
-                    showMySheet();
-                  },
-                  child: CircleAvatar(
-                    //backgroundColor: Colors.black,
-                    radius: 60.0,
-                    backgroundImage: userImage == null
-                        ? const AssetImage('assets/images/user.png')
-                        : MemoryImage(userImage!) as ImageProvider,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Add Name',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: 'Add Number',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                TextFormField(
-                  maxLines: 10,
-                  decoration: InputDecoration(
-                    hintText: 'Add Description',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStatePropertyAll(
-                      Colors.green,
+            //Wrapped column with Form widget
+            child: Form(
+              //created _formKey
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      showMySheet();
+                    },
+                    child: CircleAvatar(
+                      //backgroundColor: Colors.black,
+                      radius: 60.0,
+                      backgroundImage: userImage == null
+                          ? const AssetImage('assets/images/user.png')
+                          : MemoryImage(userImage!) as ImageProvider,
                     ),
                   ),
-                  child: Text(
-                    'Save',
-                    style: TextStyle(
-                      color: Colors.white,
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  TextFormField(
+                    //added validator
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return '*please add name';
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'Add Name',
+                      border: OutlineInputBorder(),
                     ),
                   ),
-                )
-              ],
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    //added validator
+                    validator: (value) {
+                      if (value!.isEmpty ||
+                          value.length < 11 ||
+                          value.length > 11 ||
+                          !value.startsWith('03')) {
+                        return '*please add correct number';
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'Add Number',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  TextFormField(
+                    maxLines: 10,
+                    //added validator
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return '*please add description';
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'Add Description',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      //checked form current state
+                      if (_formKey.currentState!.validate()) {
+                        //showed snackbar
+                        ScaffoldMessenger.of(context).showSnackBar(
+                         const  SnackBar(
+                            content: Text(
+                              'Form Validated',
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                    style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                        Colors.green,
+                      ),
+                    ),
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
@@ -147,7 +188,7 @@ class _AddFriendsScreenState extends State<AddFriendsScreen> {
                             Navigator.pop(context);
                           });
                         },
-                        icon: Icon(
+                        icon: const Icon(
                           Icons.photo_album,
                           size: 30.0,
                         ),
@@ -168,6 +209,9 @@ class _AddFriendsScreenState extends State<AddFriendsScreen> {
       return;
     } else {
       userImage = await sourceImage.readAsBytes();
+
+      print(
+          '........................... this is user Image ${userImage.toString()}');
       setState(() {});
     }
   }
