4626daca4abb7491246384c515b85eed226b6a9f
AbuBakar044 <bhattiabubakar@gmail.com>
2023-11-14 21:14:22 +0500

    Completed app by adding phone call functionality
    Completed app by adding phone call functionality

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:hive/hive.dart';
 import 'package:saddam_app/add_friends_screen.dart';
 import 'package:saddam_app/models/friend_model.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:whatsapp_unilink/whatsapp_unilink.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -28,7 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         backgroundColor: Colors.amber,
         centerTitle: true,
-        title: const Text('Home Screen'),
+        title: const Text('My Friends'),
       ),
       //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
@@ -50,33 +52,64 @@ class _HomeScreenState extends State<HomeScreen> {
             valueListenable: friendBox!.listenable(),
             builder: (context, _, __) {
               friendsList = friendBox!.values.toList().cast<FriendModel>();
-              return ListView.builder(
-                itemCount: friendsList.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      tileColor: Colors.amber,
-                      onLongPress: () {
-                        showDeleteDialog(index);
+              return friendsList.isEmpty
+                  ? const Center(
+                      child: Text(
+                        "You don't have any friends \n press (+) button to add",
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  : ListView.builder(
+                      itemCount: friendsList.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            tileColor: Colors.amber,
+                            onLongPress: () {
+                              showDeleteDialog(index);
+                            },
+                            leading: CircleAvatar(
+                              radius: 30.0,
+                              backgroundImage: MemoryImage(
+                                friendsList[index].friendImage!,
+                              ),
+                            ),
+                            title: Row(
+                              children: [
+                                Expanded(
+                                    //flex: 2,
+                                    child: Text(
+                                  friendsList[index].name!,
+                                  textAlign: TextAlign.left,
+                                  //overflow: TextOverflow.ellipsis,
+                                )),
+                                //Spacer(),
+                                Expanded(
+                                    //flex: 1,
+                                    child: Text(
+                                  friendsList[index].mobile!,
+                                  textAlign: TextAlign.right,
+                                )),
+                              ],
+                            ),
+                            subtitle: Row(
+                              children: [
+                                Expanded(child: Text(friendsList[index].desc!)),
+                                IconButton(
+                                  onPressed: () {
+                                    showCallDialog(index);
+                                  },
+                                  icon: const Icon(
+                                    Icons.call,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
                       },
-                      leading: CircleAvatar(
-                        backgroundImage: MemoryImage(
-                          friendsList[index].friendImage!,
-                        ),
-                      ),
-                      title: Row(
-                        children: [
-                          Text(friendsList[index].name!),
-                          Spacer(),
-                          Text(friendsList[index].mobile!),
-                        ],
-                      ),
-                      subtitle: Text(friendsList[index].desc!),
-                    ),
-                  );
-                },
-              );
+                    );
             }),
       ),
     );
@@ -125,8 +158,54 @@ class _HomeScreenState extends State<HomeScreen> {
         });
   }
 
+  Future<void> showCallDialog(int index) async {
+    await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Call Friend'),
+            content: const Text('Choose a method to call your friend'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  callMyFriend(friendsList[index].mobile!);
+                },
+                child: const Text(
+                  'Phone',
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  whatsAppMyFriend(friendsList[index].mobile!);
+                },
+                child: const Text(
+                  'WhatsApp',
+                  style: TextStyle(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
   Future<void> deleteFriend(int index) async {
     friendsList.removeAt(index);
     await friendBox!.deleteAt(index);
+  }
+
+  Future<void> callMyFriend(String number) async {
+    await launch('tel:$number');
+  }
+
+  Future<void> whatsAppMyFriend(String number) async {
+    final whatsAppNumber = number.replaceFirst(RegExp(r'0'), '+92');
+    final link =
+        WhatsAppUnilink(phoneNumber: whatsAppNumber, text: 'Hy my friend');
+    await launch(link.toString());
   }
 }
